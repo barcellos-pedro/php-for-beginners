70ce194d4eefe4e3dc10b10aafea57ea3b000b9e
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2024-09-11 22:22:33 -0300

    Page links - 12

--- demo/about.view.php
+++ demo/about.view.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Demo</title>
+    <title>About</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="h-full">
@@ -18,10 +18,10 @@
                     </div>
                     <div class="hidden md:block">
                         <div class="ml-10 flex items-baseline space-x-4">
-                            <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
-                               aria-current="page">Home</a>
-                            <a href="/about.php"
-                               class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
+                            <a href="/"
+                               class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Home</a>
+                            <a href="/about.php" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
+                               aria-current="page">About</a>
                             <a href="/contact.php"
                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
                         </div>
@@ -154,13 +154,13 @@
 
     <header class="bg-white shadow">
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+            <h1 class="text-3xl font-bold tracking-tight text-gray-900">About</h1>
         </div>
     </header>
 
     <main>
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <p>Welcome to the Home page</p>
+            <p>Welcome to the About page</p>
         </div>
     </main>
 </div>
