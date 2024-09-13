2ff711cf3c4c9435880a782f435a60f823193a40
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2024-09-13 20:18:06 -0300

    Extract Database Class - 18

---
+++ demo/Database.php
@@ -0,0 +1,20 @@
+<?php
+
+class Database
+{
+    public $connection;
+
+    public function __construct()
+    {
+        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4;user=root;password=admin";
+
+        $this->connection = new PDO($dsn);
+    }
+
+    public function query($query)
+    {
+        $statement = $this->connection->prepare($query);
+        $statement->execute();
+        return $statement;
+    }
+}
--- demo/index.php
+++ demo/index.php
@@ -2,15 +2,10 @@
 
 require 'functions.php';
 //require 'router.php';
+require 'Database.php';
 
-// connect to our MySQL Database
-$dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4;user=root;password=admin";
-$pdo = new PDO($dsn);
-
-$statement = $pdo->prepare('SELECT * FROM posts');
-$statement->execute();
-
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+$db = new Database();
+$posts = $db->query('SELECT * FROM posts')->fetchAll(PDO::FETCH_ASSOC);
 
 foreach ($posts as $post) {
     echo "<p>{$post['id']} - {$post['title']}</p>";
